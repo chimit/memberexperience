4a7a896cba5bbff9288c3774eb16934af12dd27e
Chimit <chimit@users.noreply.github.com>
2023-02-20 22:55:53 +0800

    Initial

---
+++ hooks/main.php
@@ -0,0 +1,26 @@
+//<?php
+
+class hook1 extends _HOOK_CLASS_
+{
+
+/* !Hook Data - DO NOT REMOVE */
+public static function hookData() {
+ return array_merge_recursive( array (
+  'postContainer' => 
+  array (
+    0 => 
+    array (
+      'selector' => 'article > aside.ipsComment_author.cAuthorPane.ipsColumn.ipsColumn_medium.ipsResponsive_hidePhone > ul.cAuthorPane_info.ipsList_reset > li[data-role=\'stats\'].ipsMargin_top',
+      'type' => 'add_inside_start',
+      'content' => '{{$joinDate = $comment->author()->get_joined(); $currentDate = new \DateTime(); $interval = $joinDate->diff($currentDate); $experience = \IPS\DateTime::formatInterval($interval, 2);}}
+<li class="ipsType_light">{lang="memberexperience_experience"}: {$experience}</li>',
+    ),
+  ),
+), parent::hookData() );
+}
+/* End Hook Data */
+
+
+
+
+}
